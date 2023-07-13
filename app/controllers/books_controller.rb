3d43c90d4d3205f40eda2e2f68bff8032593f564
hideo333666 <137790586+hideo333666@users.noreply.github.com>
2023-07-13 02:12:12 +0000

    [Update] 4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-
+    flash[:notice] = "You have created book successfully."
     if @book.save
       redirect_to book_path(@book.id)
     else
@@ -36,7 +36,9 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    flash[:notice] = 'updated book successfully.'
+    flash[:notice] = "You have updated book successfully."
+
+
     
     if @book.save
       redirect_to book_path(@book.id)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def update
    @user = User.find(params[:id])
-   flash[:notice] = 'update successfully.'
+   flash[:notice] = 'You have updated user successfully.'
 
    if @user.update(user_params)
      redirect_to user_path(@user.id)
